a035268a21af2f22ac491186c21651ee10ea43a4
Christian Simon <csimon1127@gmail.com>
2020-11-24 23:34:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,11 +12,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON subgenres.id = authors.id "
+  "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON subgenres.id = authors.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series GROUP BY series.title HAVING (SELECT COUNT(characters.species) FROM characters) LIMIT 1;"
+  "SELECT series.title from series JOIN "
 end
 
 def select_character_names_and_number_of_books_they_are_in
